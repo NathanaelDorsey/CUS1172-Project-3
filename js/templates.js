b45319b79206041b337a83a5ac1a3816e7bad993
NateDorsey <nathanael.dorsey20@my.stjohns.edu>
2024-03-28 14:27:58 -0400

    Made changes to db.json, app.js, index.html and templates.js

---
+++ js/templates.js
@@ -0,0 +1,7 @@
+function getQuestionTemplate() {
+    const templateScript = document.getElementById('question-template').innerHTML;
+    return Handlebars.compile(templateScript);
+}
+
+
+window.getQuestionTemplate = getQuestionTemplate;
